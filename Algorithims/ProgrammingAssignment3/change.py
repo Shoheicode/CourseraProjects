1cff4579ea3ac4c9eb56942c04211bb1f50be585
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-06 00:25:33 -0700

    updated change greedy algorithim

--- Algorithims/ProgrammingAssignment3/change.py
+++ Algorithims/ProgrammingAssignment3/change.py
@@ -1,14 +1,14 @@
 def change(money):
     change = 0
-    while money > 10:
+    while money >= 10:
         money = money-10
         change +=1
 
-    while money > 5:
+    while money >= 5:
         money = money -5
         change+=1
     
-    while money > 0:
+    while money >= 1:
         money = money -1
         change +=1
 
