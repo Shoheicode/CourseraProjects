0434334c333e4640afa3f3d31c6f7f41d158737e
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-06 14:53:51 -0700

    updated car fueling algorithim

--- Algorithims/ProgrammingAssignment3/car_fueling.py
+++ Algorithims/ProgrammingAssignment3/car_fueling.py
@@ -2,8 +2,26 @@ from sys import stdin
 
 
 def min_refills(distance, tank, stops):
+    stops.append(distance)
+    num_refills = 0
+    current_pos =0
+    last_refill_pos = 0
+
+    for i in range(len(stops)):
+        # Check if it's possible to reach the next stop or destination
+        if stops[i] - current_pos > tank:
+            # If we can't reach the next stop after the last refill, refill at the last stop
+            if current_pos == last_refill_pos:
+                # If the car is at the same place where it last refilled and can't reach the next stop, it's impossible
+                return -1  # Impossible to reach the destination
+            # Refill at the last stop
+            num_refills += 1
+            last_refill_pos = current_pos
+        
+        # Move the car to the next stop
+        current_pos = stops[i]
     # write your code here
-    return -1
+    return num_refills
 
 
 if __name__ == '__main__':
