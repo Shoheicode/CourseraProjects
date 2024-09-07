4c6680d9f11549a0fab364473044b0463fb14d05
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-07 11:08:42 -0700

    Implemented Binary Search

---
+++ Algorithims/ProgrammingAssignment4/binary_search.py
@@ -0,0 +1,23 @@
+def binary_search(arr, x):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+if __name__ == '__main__':
+    num_keys = int(input())
+    input_keys = list(map(int, input().split()))
+    assert len(input_keys) == num_keys
+
+    num_queries = int(input())
+    input_queries = list(map(int, input().split()))
+    assert len(input_queries) == num_queries
+
+    for q in input_queries:
+        print(binary_search(input_keys, q), end=' ')
