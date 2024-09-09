44383fbcb7fb3bb54da04a761fdab24aded9d24e
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-09 14:50:42 -0700

    added new file

---
+++ Data_Structures/Assignment1/check_brackets.py
@@ -0,0 +1,33 @@
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(next)
+
+        if next in ")]}":
+            # Process closing bracket, write your code here
+            print(opening_brackets_stack.pop())
+    
+    return False
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    # Printing answer, write your code here
+
+
+if __name__ == "__main__":
+    main()
