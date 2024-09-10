372d81cba8108670d71b9961309b87782d55b149
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-10 13:21:43 -0700

    added reachability

--- Algorithims/ProgrammingAssignment2/gcd.py
+++ Algorithims/ProgrammingAssignment2/gcd.py
@@ -1,11 +1,4 @@
 def gcd(a, b):
-    # current_gcd = 1
-    # for d in range(2, min(a, b) + 1):
-    #     if a % d == 0 and b % d == 0:
-    #         if d > current_gcd:
-    #             current_gcd = d
-
-    # return current_gcd
     if b == 0:
         return a
     aPrime = a%b
