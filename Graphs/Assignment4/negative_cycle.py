ee221f83281ee256f35b0f57db5e63ea947c4832
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-15 13:42:55 -0700

    currently stuck on algorithm

--- Graphs/Assignment4/negative_cycle.py
+++ Graphs/Assignment4/negative_cycle.py
@@ -2,7 +2,7 @@
 
 import sys
 
-def negative_cycle(adj, cost):
+def negative_cycle(n, edges):
     # dis = [float('inf')] * len(adj)
     # prev = [None] * len(adj)
     
@@ -29,11 +29,12 @@ def negative_cycle(adj, cost):
     #         if dis[i] != float('inf') and dis[i] + w < dis[adj[i][j]]:
     #             return 1
     # Step 1: Initialize distances
+
     dist = [float('inf')] * n
     dist[0] = 0  # Start from an arbitrary vertex, here vertex 1 (index 0)
     
     # Step 2: Relax all edges n-1 times
-    for _ in range(n - 1):
+    for _ in range(n-1):
         for u, v, w in edges:
             if dist[u] != float('inf') and dist[u] + w < dist[v]:
                 dist[v] = dist[u] + w
@@ -53,15 +54,3 @@ if __name__ == '__main__':
         edges.append((u - 1, v - 1, w))  # Convert to 0-based index
     
     print(negative_cycle(n, edges))
-    # input = sys.stdin.read()
-    # data = list(map(int, input.split()))
-    # n, m = data[0:2]
-    # data = data[2:]
-    # edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
-    # data = data[3 * m:]
-    # adj = [[] for _ in range(n)]
-    # cost = [[] for _ in range(n)]
-    # for ((a, b), w) in edges:
-    #     adj[a - 1].append(b - 1)
-    #     cost[a - 1].append(w)
-    # print(negative_cycle(adj, cost))
