e2c0cf3583d3bae1397bb216f1b58b99d2445e70
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-15 22:02:34 -0700

    added comments to make code clearer

--- Algorithims/ProgrammingAssignment1/another_max_pairwise_product.py
+++ Algorithims/ProgrammingAssignment1/another_max_pairwise_product.py
@@ -3,23 +3,27 @@ def max_pairwise_product(numbers):
     max_product = 0
     max = 0
     maxIndex = 0
-    
+
+    #Finds the first value that is the highest number
     for first in range(n):
         if max < numbers[first]:
             maxIndex = first
             max = numbers[first]
 
+    #Swaps it for the last number in the list
     numbers[maxIndex] = numbers[n-1]
     numbers[n-1] = max
 
     max = 0
     maxIndex = 0
 
+    #finds the second highest number
     for second in range(n-1):
         if max < numbers[second]:
             maxIndex = second
             max = numbers[second]
 
+    #Swaps it with the second to last number in the list
     numbers[maxIndex] = numbers[n-2]
     numbers[n-2] = max
 
