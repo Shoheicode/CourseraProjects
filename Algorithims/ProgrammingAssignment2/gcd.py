d77c0851e183e4ac2ea53ea4595c00b1abc8914e
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-16 16:31:57 -0700

    added an example of a naive function for solving gcd

--- Algorithims/ProgrammingAssignment2/gcd.py
+++ Algorithims/ProgrammingAssignment2/gcd.py
@@ -1,5 +1,13 @@
 #This program can work on large numbers
 
+# Naive Function (a,b)
+# best = 0
+# for d from 1 to a+b
+#     if d|a and d|b:
+#         best = d
+# return best
+
+
 def gcd(a, b):
     # Base case, when b == 0, it means that a is the lowest gcd
     if b == 0:
