de69477b281a3a9095e284c6fa550a44432932d6
ShoheiCode <ucodejasonirie@gmail.com>
2024-09-17 09:50:12 -0700

    Added comments about the relationship between GCD and LCM

--- Algorithims/ProgrammingAssignment2/lcm.py
+++ Algorithims/ProgrammingAssignment2/lcm.py
@@ -2,6 +2,11 @@
 # The LCM of two numbers is defined as the smallest integer which is a multiple of both integers. 
 # LCM of an array is the smallest possible integer that is multiple of all the elements of the array.
 
+# Relation between GCD and LCM:
+# Consider two integers a and b and their GCD be gcd(a, b) and LCM be lcm(a, b) . Therefore,
+
+# a * b = gcd(a, b) * lcm(a, b)
+
 # Needed to help find the Least Common Multiple
 def gcd(a, b):
     if b == 0:
@@ -12,6 +17,7 @@ def gcd(a, b):
 
 # This is the algorithm used to find the least common multiple
 def lcm(a, b):
+
     return (a // gcd(a,b))*b
 
 if __name__ == '__main__':
