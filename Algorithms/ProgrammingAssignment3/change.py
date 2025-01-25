198c0b8e29386c0f7fb1973c1aec341b3c45f6bd
ShoheiCode <ucodejasonirie@gmail.com>
2025-01-25 00:38:27 -0800

    update greedy algortithm

--- Algorithms/ProgrammingAssignment3/change.py
+++ Algorithms/ProgrammingAssignment3/change.py
@@ -1,20 +1,21 @@
 def change(money):
+    # Calculates Change through greedy algorithm
     change = 0
     while money >= 10:
-        money = money-10
-        change +=1
+        money = money - 10
+        change += 1
 
     while money >= 5:
-        money = money -5
-        change+=1
-    
+        money = money - 5
+        change += 1
+
     while money >= 1:
-        money = money -1
-        change +=1
+        money = money - 1
+        change += 1
 
     return change
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     m = int(input())
     print(change(m))
